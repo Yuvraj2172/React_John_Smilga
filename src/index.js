4865e3c0d77ac9aba8716f8904e6b789c79ce5bf
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-18 16:48:20 +0530

    Props Setup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+const title = "Atomic Habits";
+const author = "James Clear";
+const img =
+  "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg";
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={img} title={title} author={author} />
+      <Book img={img} title={title} author={author} />
+      <Book img={img} title={title} author={author} />
+      <Book img={img} title={title} author={author} />
     </section>
   );
 };
-const Book = () => {
-  const title = 'Atomic Habits'
-  const author = 'James Clear';
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg" />
-      <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x=6}</p> */}
-      <p>{6+6}</p>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author.toUpperCase()}</h4>
     </article>
   );
 };

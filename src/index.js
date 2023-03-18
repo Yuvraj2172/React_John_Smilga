fc12f14a0c37e4acaea9fcbb5c86959bb87a3781
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-18 20:28:49 +0530

    Proper List

--- src/index.js
+++ src/index.js
@@ -12,6 +12,15 @@ const books=[
     title: "Energize Your Mind",
     author: "Gaur Gopal Das",
     img: "https://d1b14unh5d6w7g.cloudfront.net/1728265371.01.S001.LXXXXXXX.jpg?Expires=1679232985&Signature=L~sXCScOw2tanW8glFuRZ0qFczTgC9T-ZSQRjTQP6cC29EA3a~wweHVkR-XxBIWjHIbJxLHJLO9kzLol9a9cpiXel9Kv0Oy0l0PSBJ98mB5kzOorIar2RH7oVh3deMJXSOZhF98xfRYXEwaBYL69Qekv-sBctFHd9tXcTqjC6tg_&Key-Pair-Id=APKAIUO27P366FGALUMQ",
+  },{
+    title: " Atomic Habits",
+    author: "James Clear",
+    img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+  },
+  {
+    title: "Energize Your Mind",
+    author: "Gaur Gopal Das",
+    img: "https://d1b14unh5d6w7g.cloudfront.net/1728265371.01.S001.LXXXXXXX.jpg?Expires=1679232985&Signature=L~sXCScOw2tanW8glFuRZ0qFczTgC9T-ZSQRjTQP6cC29EA3a~wweHVkR-XxBIWjHIbJxLHJLO9kzLol9a9cpiXel9Kv0Oy0l0PSBJ98mB5kzOorIar2RH7oVh3deMJXSOZhF98xfRYXEwaBYL69Qekv-sBctFHd9tXcTqjC6tg_&Key-Pair-Id=APKAIUO27P366FGALUMQ",
   }
 ]
 const names=['yuvraj','soni', 'uveee'];
@@ -22,17 +31,16 @@ console.log(newNames);
 const BookList = () => {
   return (
     <section className="booklist">
-     {/* {books} */}
-     {/* {newNames} */}
-     {names.map((name)=>{
-      return <h1>{name}</h1>
-     })}
+    {books.map((book)=>{
+      const {img , title,author , children} = book;
+      return <Book img={img} title ={title} author = {author}  children = {children}/>
+    })}
     </section>
   );
 };
 const Book = (props) => {
-  const {img , title , author,children} = props;
-  console.log(props)
+  // const {img , title , author,children} = props;
+  // console.log(props)
   return (
     <article className="book">
       <img src={img} alt={title} />

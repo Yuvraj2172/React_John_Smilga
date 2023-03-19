6049f7986e679f5dd622c999fb5536ad2f1dbafb
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-19 16:38:38 +0530

    Prop Drilling

--- src/index.js
+++ src/index.js
@@ -16,15 +16,15 @@ const books = [
     id: 2,
   },
   {
-    title: " Atomic Habits",
-    author: "James Clear",
-    img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    title: " The Psychology of Money",
+    author: "Morgan Housel",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL600_SR600,400_.jpg",
     id: 3,
   },
   {
-    title: "Energize Your Mind",
-    author: "Gaur Gopal Das",
-    img: "https://d1b14unh5d6w7g.cloudfront.net/1728265371.01.S001.LXXXXXXX.jpg?Expires=1679232985&Signature=L~sXCScOw2tanW8glFuRZ0qFczTgC9T-ZSQRjTQP6cC29EA3a~wweHVkR-XxBIWjHIbJxLHJLO9kzLol9a9cpiXel9Kv0Oy0l0PSBJ98mB5kzOorIar2RH7oVh3deMJXSOZhF98xfRYXEwaBYL69Qekv-sBctFHd9tXcTqjC6tg_&Key-Pair-Id=APKAIUO27P366FGALUMQ",
+    title: "Grandma's Bag of Stories: Collection of 20+",
+    author: "Héctor García",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/814L+vq01mL._AC_UL600_SR600,400_.jpg",
     id: 4,
   },
 ];
@@ -35,13 +35,17 @@ const newNames = [...names, "yuvrrraj"];
 //   return <h1>{name}</h1>
 // });
 const BookList = () => {
+  const someValue = 'shakeAndBake';
+  const displayValue = ()=>{
+    console.log(someValue);
+  }
   return (
     <>
       <section className="booklist">
         <EventExamples />
         {books.map((book) => {
           // const {img , title,author , children,id} = book;
-          return <Book {...book} key={book.id} />;
+          return <Book {...book} key={book.id} displayValue={displayValue} />;
         })}
       </section>
     </>
@@ -90,7 +94,7 @@ const EventExamples = () => {
 };
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author, children, displayValue } = props;
   // console.log(props)
   return (
     <article className="book">
@@ -98,7 +102,7 @@ const Book = (props) => {
       <h2>{title}</h2>
       {children}
       <h4>{author.toUpperCase()}</h4>
-      <button style={{ margin: "0.5rem", padding: "0.5rem" }} onClick={()=>{ console.log(title)}} >Display Title</button>
+      <button style={{ margin: "0.5rem", padding: "0.5rem" }} onClick={displayValue} >Display Title</button>
     </article>
   );
 };
